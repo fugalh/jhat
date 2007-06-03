efc7cc5ea0c9a86b5f9a4e2bd15393dcc722821f
hans <hans@fugal.net>
2007-06-02 23:56:51 -0600

    xplane.sh
    darcs-hash:20070603055651-28bda-d0967a009637da06c376e42a36109d4fa29129e2.gz

---
+++ xplane.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+# A unique grep pattern for your joystick. See /proc/bus/input/devices.
+name="YOKE"
+
+# The left/right axis, and the up/down axis. Use jstest to find this out.
+xaxis=5
+zaxis=6
+
+# Executable paths
+jhat=~/bin/jhat
+xplane=~/xplane/X-Plane-athlon-xp
+
+## end configuration
+
+for d in /sys/class/input/js*; do
+    if grep -q "$name" $d/device/input:input*/name 2>/dev/null; then
+	dev=/dev/input/`basename $d`
+    fi
+done
+
+if [ -z "$dev" ]; then
+    echo "Alas, the joystick to match '$name' was not found."
+    exit 1
+fi
+
+$jhat $dev $xaxis $zaxis &
+pid=$!
+
+cd `dirname "$xplane"`
+"$xplane"
+
+kill $pid
